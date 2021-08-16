3cd0fc917bc6e14222ce63725bdeeda29249eda9
geniyMsk <83307950+geniyMsk@users.noreply.github.com>
2021-08-16 23:01:45 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,14 +1,38 @@
+# -*- coding: utf8 -*-
+
+
 import logging
 from aiogram import executor
 from utils import on_startup_notify
 from loader import dp, scheduler
 import handlers
+from aiogram.types import ParseMode, BotCommand, BotCommandScopeChat
+from config import ADMINS
+from loader import scheduler, bot
+
+import dir.DBCommands as db
+import dir.keyboard as kb
 
 
 async def on_startup(dispatcher):
     await on_startup_notify(dispatcher)
-    #schedule_jobs()
+    jobs = db.get_all_job()
+    for job in jobs:
+        async def notify():
+            phrase = db.get_random_notify_phrase()
+            await bot.send_message(job.chat_id, phrase, reply_markup=kb.progress, parse_mode=ParseMode.HTML)
 
+        hour = job.time.hour
+        minute = job.time.minute
+        scheduler.add_job(notify, "cron", hour=hour, minute=minute)
+
+
+    for admin in ADMINS:
+        try:
+            await bot.set_my_commands(commands=[BotCommand(command='logging', description='Выгрузка логирования')],
+                                      scope=BotCommandScopeChat(chat_id=admin))
+        except Exception as error:
+            logging.exception(error)
 
 log_file = 'logs.log'
 f = open(log_file, 'a')
--- utils.py
+++ utils.py
@@ -13,19 +13,9 @@ import dir.keyboard as kb
 async def on_startup_notify(dp: Dispatcher):
     for admin in ADMINS:
         try:
-            await dp.bot.send_message(admin, "Бот Запущен")
-            pass
+            await bot.send_message(admin, "Бот Запущен")
         except Exception as err:
             logging.exception(err)
-    jobs = db.get_all_job()
-    for job in jobs:
-        async def notify():
-            phrase = db.get_random_notify_phrase()
-            await bot.send_message(job.chat_id, phrase, reply_markup=kb.progress, parse_mode=ParseMode.HTML)
-        hour = job.time.hour
-        minute = job.time.minute
-        scheduler.add_job(notify, "cron", hour = hour, minute = minute)
-    await bot.set_my_commands(commands=[BotCommand(command='logging', description='Выгрузка логирования')],
-                              scope=BotCommandScopeChat(chat_id=admin))
+
 
 
